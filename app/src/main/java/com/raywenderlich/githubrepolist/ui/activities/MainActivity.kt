6d52a4b0a6091ce449f09eac407b6e06a223b9dd
abunur <fuad@anaara.com>
2017-12-12 01:40:05 -0500

    android studio updates
    Signed-off-by: abunur <fuad@anaara.com>

--- app/src/main/java/com/raywenderlich/githubrepolist/ui/activities/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/githubrepolist/ui/activities/MainActivity.kt
@@ -3,13 +3,19 @@ package com.raywenderlich.githubrepolist.ui.activities
 import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 import com.raywenderlich.githubrepolist.R
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.find
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : Activity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
 
 
   }
