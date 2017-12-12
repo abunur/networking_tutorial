b30c58c20f17a82a780b99187cac19e53c1c173f
abunur <fuad@anaara.com>
2017-12-12 02:23:30 -0500

    first successful network request
    dump github repo search to Logcat
    Not updating UI with it yet
    
    Signed-off-by: abunur <fuad@anaara.com>

--- app/src/main/java/com/raywenderlich/githubrepolist/ui/activities/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/githubrepolist/ui/activities/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.raywenderlich.githubrepolist.ui.activities
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.raywenderlich.githubrepolist.R
+import com.raywenderlich.githubrepolist.data.Request
+import com.raywenderlich.githubrepolist.ui.adapters.RepoListAdapter
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
 import org.jetbrains.anko.longToast
@@ -12,11 +16,29 @@ import org.jetbrains.anko.uiThread
 
 class MainActivity : Activity() {
 
+  private val items = listOf(
+      "JetBrains/kotlin - The Kotlin Programming Language",
+      "exercism/kotlin - Exercism exercises in Kotlin",
+      "cbeust/kobalt - A Kotlin-based build system for the JVM",
+      "JetBrains/kotlin - The Kotlin Programming Language",
+      "exercism/kotlin - Exercism exercises in Kotlin",
+      "cbeust/kobalt - A Kotlin-based build system for the JVM",
+      "JetBrains/kotlin - The Kotlin Programming Language"
+  )
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    val repoList = find<RecyclerView>(R.id.repo_list)
+    repoList.layoutManager = LinearLayoutManager(this)
+    repoList.adapter = RepoListAdapter(items)
+    //search for popular tetris repositories written in Kotlin
+    val url = "https://api.github.com/search/repositories?q=tetris+language:kotlin&sort=stars&order=desc"
 
-
+    doAsync {
+      Request(url).run()
+      uiThread { longToast("Request performed") }
+    }
   }
 }
--- app/src/main/java/com/raywenderlich/githubrepolist/ui/adapters/RepoListAdapter.kt
+++ app/src/main/java/com/raywenderlich/githubrepolist/ui/adapters/RepoListAdapter.kt
@@ -1,7 +1,25 @@
 package com.raywenderlich.githubrepolist.ui.adapters
 
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
 /**
  * Created by abunur on 12/12/17.
  */
-class RepoListAdapter(private val items: List<String>) {
+class RepoListAdapter(private val items: List<String>)
+  :RecyclerView.Adapter<RepoListAdapter.ViewHolder>() {
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+      ViewHolder(TextView(parent.context))
+
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    holder.textView.text = items[position]
+  }
+
+  override fun getItemCount(): Int = items.size
+
+
+
+  class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 }
